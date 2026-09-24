41caa4c64b4b527779d7289df78dbe364cefa3fc
stefan <dupontstef80@gmail.com>
2026-09-24 11:24:08 +0200

    feat (visaulize light state)

--- scripts/run.py
+++ scripts/run.py
@@ -1,12 +1,23 @@
 import sumolib, traci
+import sys
 
-SUMO = sumolib.checkBinary("sumo")  # "sumo-gui" pour voir la simulation
-traci.start([SUMO, "-c", "simulation/carrefour.sumocfg"])
+
+GUI = "--gui" in sys.argv
+SUMO = sumolib.checkBinary("sumo-gui" if GUI else "sumo")
+
+cmd = [SUMO, "-c", "simulation/carrefour.sumocfg"]
+if GUI:
+    cmd += ["--start", "--delay", "100", "--quit-on-end"]
+
+traci.start(cmd)
 
 while traci.simulation.getMinExpectedNumber() > 0:
     traci.simulationStep()
+    traci.trafficlight.setRedYellowGreenState("C", state="rrrrrrrrrrrrrrrrrr")
     t = traci.simulation.getTime()
     if t % 300 == 0:
+        NL = traci.trafficlight.getRedYellowGreenState("C")
+        print(f"North traffic Light state ${NL}")
         attente = sum(traci.edge.getWaitingTime(e) for e in ["O2C", "N2C", "S2C", "E2C"])
         print(f"t={t:.0f}s  véhicules={traci.vehicle.getIDCount()}  attente={attente:.0f}s")
 
